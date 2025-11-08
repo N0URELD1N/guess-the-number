48c06086d41631e8c0c655b08986a0e5c5d4c5f2
N0URELD1N <nom22646@gmail.com>
2025-11-09 00:21:35 +0300

    Translate messages from Arabic to English

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function checkGuess() {
   const message = document.getElementById("message");
   
   if (isNaN(guess) || guess < 1 || guess > 100) {
-    message.textContent = "الرجاء إدخال رقم صالح بين 1 و 100";
+    message.textContent = "Please write a number betwen 1-100";
     message.style.color = "red";
     return;
   }
@@ -20,13 +20,13 @@ function checkGuess() {
   attempts++;
 
   if (guess === randomNumber) {
-    message.textContent = `أحسنت! لقد خمنت الرقم في ${attempts} محاولة`;
+    message.textContent = `Congratulations! You guessed the number in ${attempts} attempt`;
     message.style.color = "green";
   } else if (guess < randomNumber) {
-    message.textContent = "الرقم أكبر من ذلك!";
+    message.textContent = "The number is bigger than the number you guessed!";
     message.style.color = "blue";
   } else {
-    message.textContent = "الرقم أصغر من ذلك!";
+    message.textContent = "The number is lower than the number you guessed !";
     message.style.color = "blue";
   }
 }
